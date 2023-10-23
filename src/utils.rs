739deb0d2700a42ff5e24beb76f648482887f248
reecehuff <reecedhuff@gmail.com>
2023-10-22 19:08:10 -0700

    minimax officially works!
    - Need to remove the enodes as it the Runner is running
    - Need to merge the MaxApplier and MinApplier into a single Applier
    - Need make the generate tree more generalizeable

--- src/utils.rs
+++ src/utils.rs
@@ -1,30 +1,10 @@
 use egg::{*, rewrite as rw}; 
 
-pub fn test_code(){
-    use egg::{*, SymbolLang as S};
-    let mut egraph = EGraph::<S, ()>::default();
-    let x = egraph.add(S::leaf("x"));
-    let y = egraph.add(S::leaf("y"));
-    let plus = egraph.add(S::new("+", vec![x, y]));
-    let plus_recexpr = "(+ x y)".parse().unwrap();
-    assert_eq!(plus, egraph.add_expr(&plus_recexpr));
-}
-
 pub fn generate_tree() -> RecExpr<SimpleLanguage> {
-    // Define the expression
-    // let expr: RecExpr<SimpleLanguage> = "(list 3 4)".parse().unwrap();
+    // Define a simple expression
     // let expr: RecExpr<SimpleLanguage> = "(white (black -3 -1) (black 6 -4) )".parse().unwrap();
     let expr: RecExpr<SimpleLanguage> = "(white (black (white -4 2) (white -10 10)) (black (white 6 -2) (white 3 -5)) )".parse().unwrap();
     
-    // Return the expression 
-    expr
-}
-
-#[allow(dead_code)]
-pub fn simple_expr() -> RecExpr<SimpleLanguage> {
-    // Define the expression
-    let expr: RecExpr<SimpleLanguage> = "(/ (* x 2 ) 2)".parse().unwrap();
-
     // Return the expression 
     expr
 }
@@ -54,14 +34,6 @@ define_language! {
     }
 }
 
-impl SimpleLanguage {
-    /// Create a tuple with the given operation and children
-    pub fn new(op: impl Into<Symbol>, children: Vec<Id>) -> (Symbol, Vec<Id>) {
-        let op = op.into();
-        (op, children)
-    }
-}
-
 fn make_rules() -> Vec<Rewrite<SimpleLanguage, ()>> {
     vec![
         // Again, when considering a move as the white player (us),
@@ -74,7 +46,7 @@ fn make_rules() -> Vec<Rewrite<SimpleLanguage, ()>> {
 
         // Similarly, when considering a move as the black player (our opponent),
         // we assume they play perfectly (i.e., pick the move that minimizing the eval)
-        rw!("black-wants-min"; "(black ?a ?b)" => { MaxApplier {
+        rw!("black-wants-min"; "(black ?a ?b)" => { MinApplier {
             a: "?b".parse().unwrap(),
             b: "?a".parse().unwrap(),
         }}),
@@ -99,93 +71,45 @@ impl Applier<SimpleLanguage, ()> for MaxApplier {
         // Therefore, we can grab the two children Id's of our parent Id (matched_id)
         let a_id = subst[self.a];
         let b_id = subst[self.b];
-        println!("parent Id: {} ", matched_id);
-        println!("  has children");
-        println!("  child a: {} ", a_id);
-        println!("  child b: {} ", b_id);
 
-        // let temp: EClass<SimpleLanguage, ()> = egraph[matched_id];
-        println!("eclass at the parent Id: {:?}", egraph[matched_id]);
-
-        // Compare the two numbers; numbers being the last node by construction
+        // Isolate the evaluation and select the maximum and add it to egraph
+        // NOTE: evaluation being the last node is simply by construction
         let num_a = egraph[a_id].nodes.last().unwrap().clone();
         let num_b = egraph[b_id].nodes.last().unwrap().clone();
-        let new_num = num_a.max(num_b);
-        let new_id = egraph.add(new_num);
+        let max_id = egraph.add(num_a.max(num_b));
 
-
-        // let new_num = num_a.clone().max(num_b.clone());
-
-        // println!("a = {:?}", num_a.clone());
-        // println!("b = {:?}", num_b.clone());
-        // println!("m = {:?}", new_num.clone());
-
-        // Add the larger number to the egraph:
-        // let new_id: Id; 
-        // if num_a > num_b {
-        //     new_id = egraph.add(num_a);
-        // } else {
-        //     new_id = egraph.add(num_b);
-        // }
-
-        // // let new_id = egraph.add(SimpleLanguage::Num(0));
-        // let new_id = egraph.add(egraph[a_id].nodes[0].clone());
-        
-        // for node in egraph[a_id].nodes.iter() {
-        //     println!("node: {}", node);
-        // }
-
-        // if egraph.union(matched_id, a0b0c0) {
-        //     vec![a0b0c0]
-        // } else {
-        //     vec![]
-        // }
-
-        // vec![]
-
-        if egraph.union(matched_id, new_id) {
-            vec![new_id]
+        // Add the Id of the maximum evaluation only when it hasn't already been added
+        if egraph.union(matched_id, max_id) {
+            vec![max_id]
         } else {
             vec![]
         }
     }
 }
 
-// struct MinApplier {
-//     a: Var,
-//     b: Var
-// }
-// impl Applier<SimpleLanguage, ()> for MinApplier {
+// MinApplier is the exact same as MaxApplier but uses .min() instead of .max()
+// See https://docs.rs/egg/latest/egg/trait.Applier.html for more information
+struct MinApplier {
+    a: Var,
+    b: Var
+}
+impl Applier<SimpleLanguage, ()> for MinApplier {
 
-//     fn apply_one(&self, egraph: &mut EGraph<SimpleLanguage,()>, matched_id: Id, subst: &Subst, _: Option<&PatternAst<SimpleLanguage>>, _: Symbol) -> Vec<Id> {
-//         let a_id: Id = subst[self.a];
-//         let b_id: Id = subst[self.b];
-//         println!("a   : {:?}", &self.a);
-//         println!("b   : {:?}", &self.b);
-//         println!("a_id: {:?}", a_id);
-//         println!("b_id: {:?}", b_id);
-//         let value_a = &egraph[a_id].nodes;
-//         let value_b = &egraph[b_id].nodes;
-//         println!("a_ns: {:?}", value_a);
-//         println!("b_ns: {:?}", value_b);
+    fn apply_one(&self, egraph: &mut EGraph<SimpleLanguage,()>, matched_id: Id, subst: &Subst, _: Option<&PatternAst<SimpleLanguage>>, _: Symbol) -> Vec<Id> {
 
-//         println!("compare: {:?}", value_a > value_b);
-        
-//         let new_id: Id; 
-//         if value_a > value_b {
-//             new_id = egraph.add(SimpleLanguage::Min([b_id]));
-//         } else {
-//             new_id = egraph.add(SimpleLanguage::Min([a_id]));
-//         }
-
-//         // let a0: Id = egraph.add(SimpleLanguage::Max([new_id]));
-//         if egraph.union(matched_id, new_id) {
-//             vec![new_id]
-//         } else {
-//             vec![]
-//         }
-//     }
-// }
+        // See MaxApplier for explanation
+        let a_id = subst[self.a];
+        let b_id = subst[self.b];
+        let num_a = egraph[a_id].nodes.last().unwrap().clone();
+        let num_b = egraph[b_id].nodes.last().unwrap().clone();
+        let min_id = egraph.add(num_a.min(num_b));
+        if egraph.union(matched_id, min_id) {
+            vec![min_id]
+        } else {
+            vec![]
+        }
+    }
+}
 
 pub fn simplify(string_expr: RecExpr<SimpleLanguage>) -> RecExpr<SimpleLanguage> {
 
@@ -205,11 +129,6 @@ pub fn simplify(string_expr: RecExpr<SimpleLanguage>) -> RecExpr<SimpleLanguage>
 
     // Visualize the new E-Graph as a png
     my_runner.egraph.dot().to_png("target/new_egraph.png").unwrap();
-
-    // Print out the classes for clarity 
-    for class in my_runner.egraph.classes() {
-        println!("class {:?}", class);
-    }
 
     // Return the best expression by leaving off the semi-colon
     best_expr
