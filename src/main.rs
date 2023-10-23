a7857ff99bb6977652a0f829647a1845b5185104
reecehuff <reecedhuff@gmail.com>
2023-10-23 11:09:30 -0700

    Adding tree depth functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 /* Imports */
 use egg::*;
 
-/* Modules & scripts */
+/* Modules */
 mod utils;
-// use crate::utils::{generate_tree, simplify, SimpleLanguage};
+mod tree;
+
+/* Use's */
 use crate::utils::*;
+// use crate::utils::{simplify, SimpleLanguage};
+use crate::tree::generate_tree;
 
 /* main */
 fn main() {
-    let expr: RecExpr<SimpleLanguage> = generate_tree(); 
+    let tree_depth = 3; 
+    let expr: RecExpr<SimpleLanguage> = generate_tree(tree_depth); 
     println!("input  expression: {}", expr);
     let best_expr: RecExpr<SimpleLanguage> = simplify(expr);
     println!("output expression: {}", best_expr);
